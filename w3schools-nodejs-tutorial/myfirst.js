39ca1b35a2ae39f84db4f30392ee005a5deba35d
thanhhaudev <thanhhaudev@gmail.com>
2024-12-31 14:41:52 +0700

    http module

--- w3schools-nodejs-tutorial/myfirst.js
+++ w3schools-nodejs-tutorial/myfirst.js
@@ -5,8 +5,10 @@
 var http = require('http'); // <= require() is a built-in function to include modules
 var dt = require('./myfirstmodule'); // <= use require() to include your own module, ./ means that the module is located in the same folder as the Node.js file
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("The date and time are currently: " + dt.myDateTime());
-    res.end();
+http.createServer(function (req, res) { // <= create server object
+    res.writeHead(200, {'Content-Type': 'text/html'}); // <= write head of response
+    res.write("The date and time are currently: " + dt.myDateTime()); // <= write body of response
+    res.write("<br>");
+    res.write("req.url: " + req.url); // <= read the query string
+    res.end(); // <= end response
 }).listen(8089);
